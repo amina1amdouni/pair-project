1196e18d7c995d1fc6f0923abd91361f81e717bb
amina1amdouni <aminaamdouni01@gmail.com>
2024-01-25 11:44:34 +0100

    singleProdect & cartList done

--- my/src/App.js
+++ my/src/App.js
@@ -1,48 +1,68 @@
-// src/App.js
+// App.js
 import React, { useState } from 'react';
-import Home from './componenets/Home.js';
-
-import AddProduct from './componenets/AddProduct.js';
-
-import AllProducts from './componenets/AllProducts.js';
+import Home from './componenets/Home';
+import AddProduct from './componenets/AddProduct';
+import AllProducts from './componenets/AllProducts';
+import ProductDetail from './componenets/singleProduct';
+import ShoppingCart from './componenets/CartList';
 
 import './App.css';
 
 function App() {
-  const [view, setView] = useState('AllProducts');
+  const [view, setView] = useState('Home');
+  const [selectedProduct, setSelectedProduct] = useState(null);
+  const [cart, setCart] = useState([]);
 
   const changeView = (view) => {
     setView(view);
   };
 
+  const navigateToProductDetail = (product) => {
+    setSelectedProduct(product);
+    setView('ProductDetail');
+  };
+
+  const handleAddToCart = (selectedProduct) => {
+    setCart([...cart, selectedProduct]);
+  };
+
+  const handleDeleteItem = (index) => {
+    const updatedCart = [...cart];
+    updatedCart.splice(index, 1);
+    setCart(updatedCart);
+  };
+
   return (
     <div className="App">
       <nav className="nav">
-        <div className="nav-item is-active" onClick={() => setView("AllProducts")}>
+        <div className={`nav-item ${view === 'Home' ? 'is-active' : ''}`} onClick={() => changeView('Home')}>
           Home
         </div>
-        
-        <div className="nav-item" onClick={() => setView(" AllProducts")}>
-        AllProducts
+        <div className={`nav-item ${view === 'AllProducts' ? 'is-active' : ''}`} onClick={() => changeView('AllProducts')}>
+          AllProducts
         </div>
-        <div className="nav-item" onClick={() => setView("AddProduct")}>
+        <div className={`nav-item ${view === 'AddProduct' ? 'is-active' : ''}`} onClick={() => changeView('AddProduct')}>
           AddProduct
+        </div>
+        <div className={`nav-item ${view === 'Basket' ? 'is-active' : ''}`} onClick={() => changeView('Basket')}>
+          Basket
         </div>
         <div className="nav-item" active-color="black">
           <input type="text" />
           <button>search</button>
         </div>
+        <div>
+          <h1>Your Food Website</h1>
+        </div>
         <span className="nav-indicator"></span>
       </nav>
-      {view === "Home" && <Home />}
-      {view === "AllProducts" && <AllProducts />}
-     
-      {view === "AddProduct" && <AddProduct onProductAdded={() => changeView("AllProducts")} />}
-      <div></div>
+      {view === 'Home' && <Home />}
+      {view === 'AllProducts' && <AllProducts navigateToProductDetail={navigateToProductDetail} />}
+      {view === 'AddProduct' && <AddProduct onProductAdded={() => changeView('AllProducts')} />}
+      {view === 'ProductDetail' && <ProductDetail item={selectedProduct} onAddToCart={handleAddToCart} />}
+      {view === 'Basket' && <ShoppingCart cart={cart} onDeleteItem={handleDeleteItem} />}
     </div>
   );
 }
 
 export default App;
-
-
